f64a5af24666d20bbf16288a1d5821883a9a6852
Md Hasibur Rahman <hasib2920@hotmail.com>
2025-10-05 14:45:54 +0800

    implemented dynamic routing

--- src/Pages/PlantDetails.jsx
+++ src/Pages/PlantDetails.jsx
@@ -10,25 +10,25 @@ const PlantDetails = () => {
   useEffect(() => {
     fetch(`https://openapi.programming-hero.com/api/plant/${id}`)
       .then((res) => res.json())
-      .then((data) => console.log(data?.plants));
+      .then((data) => setPlant(data?.plants));
   }, [id]);
+
+  const { name, description, image, price, category } = plant;
+
+  console.log(name);
   return (
     <div>
       <div className="card bg-base-100 max-w-5xl mx-auto shadow-sm">
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-          />
+        <figure className="h-48 overflow-hidden">
+          <img className="w-full h-500px object-cover" src={image} />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Card Title</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
+          <h2 className="card-title">{name}</h2>
+          <p>category: {category}</p>
+          <p>description: {description} </p>
+          <p>Price: {price} </p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-primary">Add to cart</button>
           </div>
         </div>
       </div>
